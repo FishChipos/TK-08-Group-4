b33595c4cc618c0c3d226365061616939ad35bd0
Carlsson Khovis <carlkho99@gmail.com>
2025-05-14 19:29:22 +0700

    Added a basic user interface.

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 
 // Arithmetic functions.
 // TODO: Function definitions.
@@ -8,5 +10,33 @@ double multiply(double a, double b);
 double divide(double a, double b);
 
 int main() {
+    bool shouldClose = false;
+
+    puts("=== CALCULATOR ===\n");
+    
+    while (!shouldClose) {
+        char input[128];
+        double result = 0;
+
+        printf("%s\n", "Enter an expression with only one operation. (type exit to close)");
+        fgets(input, 128, stdin);
+
+        // Remove trailing newline from fgets().
+        input[strcspn(input, "\n")] = '\0';
+
+        // Check if user wants to close.
+        if (strcmp(input, "exit") == 0) {
+            shouldClose = true;
+            break;
+        }
+
+        // TODO: Input parsing.
+
+        printf("Result: %lf\n\n", result);
+    }
+
+    // End message.
+    puts("\nThank you for using our calculator!");
+
     return 0;
 }
