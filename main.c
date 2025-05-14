2630618b12a3f846dbcb158e29ce992777053e67
Carlsson Khovis <carlkho99@gmail.com>
2025-05-14 19:20:58 +0700

    Added arithmetic function declarations.

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Arithmetic functions.
+// TODO: Function definitions.
+double add(double a, double b);
+double subtract(double a, double b);
+double multiply(double a, double b);
+double divide(double a, double b);
+
+int main() {
+    return 0;
+}
